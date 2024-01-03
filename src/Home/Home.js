d916bc30d199ac67c1df11409215195b81481868
Sriramvedulla <vedsriram109@gmail.com>
2024-01-03 13:15:19 +0530

    changes done

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -30,6 +30,7 @@ function Home() {
       <div className="banner1">
         <img
           src="https://assets-in.bmscdn.com/discovery-catalog/collections/tr:w-1440,h-120:q-80/stream-leadin-web-collection-202210241242.png"
+          alt="homeimg"
           style={{
             width: "90%",
             marginLeft: "5%",
